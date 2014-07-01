4e5f15c1a91d77d65e65fbc7fb0fd0036c99bff7
Jelte Liebrand <jliebrand@google.com>
2014-06-30 21:18:15 -0700

    adding notes for table row flow algorithm

--- elements/mixin/flowCells.js
+++ elements/mixin/flowCells.js
@@ -51,6 +51,20 @@ window.FlowCells = {
         for (var i = 0; i < this.childElementCount; i++) {
           var cell = cellMetaData[i].cell;
           cell.flow(overflowingFunc);
+
+          // TODO(jliebrand): once the parent overflowingFunc returns false
+          // then we need to pass in our own overflowingfunc to the cells
+          // where they should flow for as long as their contentHeight
+          // does not exceed their own td height...
+          // TODO(jliebrand): think about how the "once no longer overflowing"
+          // works for REFLOW...
+          // SHOULD be straight forward; similar to other flow algorithms:
+          // 1- pick source (this or flowInto)
+          // 2- pick tallest cell on that source
+          // 3- flow that cell using given overflowingFunc
+          // 4- now flow the rest of the cells in tallest to shortest order
+          //    using a new overflowing func that checks the child cell's
+          //    content height against this rows own offsetHeight
         }
 
         // step 4 - normalize in case this or flowInto have ONLY empty cells
